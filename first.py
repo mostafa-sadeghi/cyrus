a24971e3dc2af95bdd32ff650314c00eeb4237cb
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-02-18 21:57:24 +0330

    sadeghi

--- first.py
+++ first.py
@@ -1,4 +1,16 @@
-print("hello cyrus")
-print("welcome to python class")
-print("we are going to learn every things about python")
-print("python from zero to hero!!!!s")
+# print("hello cyrus")
+# print("welcome to python class")
+# print("we are going to learn every things about python")
+# print("python from zero to hero!!!!s")
+##################################################
+# primitive types in python
+# numbers      str     bool
+# number = 1
+# print(type(number))
+# number = 1.5
+# print(type(number))
+
+# status = True
+# print(type(status))
+status = False
+print(type(status))
---
+++ string1.py
@@ -0,0 +1,37 @@
+# name = 'cyrus'
+# print(type(name))
+# name = "cyrus"
+# print(type(name))
+
+
+# print('cyrus')
+# print("cyrus")
+# how to naming a variable in python: any combination of:
+# alphebetical english chars and _ and number except that we can't put numbers first
+# first chars of the variable name can not be a number
+# number_one = 1
+
+
+# name = "cyrus"
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+# print(name[5])    # IndexError: string index out of range
+
+# print()
+# print('****************************************')
+# print(name[0], ' * ', name[1], ' * ', name[2], ' * ', name[3], ' * ', name[4])
+# name2 = 'mehran'
+# message = 'Hello' + ' ' + name + ' ' + name2
+# print(message)
+# print('Hello', name, name2)
+
+name = input('enter your name: ')
+print("your name is:", name)
+# print("name type is:", type(name))
+family = input('enter your family: ')
+
+message = "hello " + name + " " + family
+print(message)
