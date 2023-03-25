e6c44f657cf82e77f6fcc2a34152e9363bc471fe
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-03-25 22:01:54 +0330

    sadeghi

---
+++ ex1.py
@@ -0,0 +1,85 @@
+# print('Hello')
+# print("1+2")
+
+# score = 0
+# print("score value is:", score)
+
+# score += 1
+# print("score value is:", score)
+
+# score = score + 1
+# print("score value is:", score)
+
+# score = score - 1
+# print("score value is:", score)
+
+# score -= 1
+# print("score value is:", score)
+
+
+# print("hello\nwelcome")
+# print("c:\\new folder")
+# print("c:\new folder")
+# print('hello\twelcome')
+
+# print('hi\rhello')
+# print('hello\rhi')
+
+# print('hello every body.\rwelcome to our python class.\rwelcome to our pygame')
+# name = input('enter a name:> ')
+# if name == "cyrus":
+#     print("welcome cyrus")
+
+# x = 1
+
+# x + 1
+# x = x + 1
+
+# print(x)
+
+# X = 4
+# X = 5
+# print(X)
+# Y = 3
+# print(X/Y)
+# print(X//Y)
+
+# a = 4
+
+# b = 5
+
+# if a > b:
+#     print("1")
+
+# if a < b:
+#     print("2")
+
+# a = 4
+
+# b = 4
+# if a >= b:
+#     print("1")
+
+# if a <= b:
+#     print("2")
+
+
+# a = 4
+# b = 4
+
+# if a > b or a == b:
+#     print('a and b are equal')
+
+# write a program that takes student's age and
+# print kid if age < 8
+# print junior if 8 <= age < 12
+# print teenager if 12 <= age < 18
+# print adult if age >= 18
+name = input('enter your name:> ')
+
+age = int(input('enter your age:> '))
+
+if age <= 8:
+    print(f"{name} you are kid")
+elif 8 < age <= 12:
+    print()
