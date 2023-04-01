5ef7d130114781feda33056139fc7ae54939660e
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-01 22:00:49 +0330

    sadeghi

--- ex1.py
+++ ex1.py
@@ -75,11 +75,99 @@
 # print junior if 8 <= age < 12
 # print teenager if 12 <= age < 18
 # print adult if age >= 18
-name = input('enter your name:> ')
+# name = input('enter your name:> ')
 
-age = int(input('enter your age:> '))
+# age = int(input('enter your age:> '))
 
-if age <= 8:
-    print(f"{name} you are kid")
-elif 8 < age <= 12:
-    print()
+# if age <= 8:
+#     print(f"{name} you are kid")
+# elif 8 < age <= 12:
+#     print()
+
+
+# a = 5
+# b = 5
+
+# if a == b:
+#     print("a and b are equal")
+
+# if a != b:
+#     print("a and b are not equal")
+
+
+# a = int(input('enter first number'))
+# b = int(input('enter first number'))
+# c = int(input('enter first number'))
+
+
+# a = True
+# if a:
+#     print("ok")
+
+# if not a:
+#     print("not ok")
+
+
+# a = True
+# b = False
+
+# if a and b:
+#     print("ok")
+
+
+# a = True
+# b = False
+
+# if a or b:
+#     print("ok")
+# a = 3
+# b = 4
+# c = a == b
+# print(c)
+# if 1:  # Truethy
+#     print("one")
+# every numbers excep zero are truethy which mean they evaluate to True
+# if 0:
+#     print("zero")
+# if -1:
+#     print("-1")
+# if '':
+#     print("balalalal")
+# if ' ':
+#     print("balalalal")
+# user_name = input('enter user name:> ')
+# password = input('enter password:> ')
+
+# if user_name and password:
+#     print("ok")
+#     print("username and password is not empty.")
+
+# else:
+#     print("you must enter user name and password")
+
+#
+# a = "c"
+
+# if a == "B" or "b":
+#     print("a is equal to b. Maybe.")
+
+# if a =="B" or a =="b":
+#     print("a is equal to b.")
+
+
+USERNAME = "admin"
+PASSWORD = 'root'
+
+
+user_name = input('enter the user name:> ')
+password = input('enter the password:> ')
+
+if not user_name or not password:
+    print('user name or password is empty.')
+else:
+    if user_name == USERNAME and password == PASSWORD:
+        print("welcome.")
+        print("login success")
+        print("you are a valid user.")
+    else:
+        print("username or password is not correct.")
