98f57c9620e94209b21a31d65b21d8ee21397047
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-04-08 21:56:01 +0330

    sadeghi

---
+++ aboutloops.py
@@ -0,0 +1,77 @@
+# for i in range(5):
+#     print("I want to learn pygame...")
+#     print("OK")
+
+# print("outside...")
+
+
+# for i in range(10):
+#     print(i, end=' ')
+
+# for i in range(1,11):
+#     print(i, end=' ')
+
+# for i in range(1,11,2):
+#     print(i, end=' ')
+# print()
+
+# for i in range(2,11,2):
+#     print(i, end=' ')
+
+
+# for i in range(10,0,-1):
+#     print(i, end=' ')
+
+
+# for i in [2,3,4,6,7,8,0,9,-1]:
+#     print(i * 2, end=' ')
+
+# for s in 'cyrus':
+#     print(s, end=' ')
+
+# for a in (1,2,3,6,8,9):
+#     print(a**2, end=' ')
+
+# for x in {1, 2, 3, 4, 8, 9, 0}:
+#     print(x, end=' ')
+
+# Nesting loops
+
+# for i in range(3):
+#     print("a")
+
+# for j in range(3):
+#     print("b")
+
+
+# for i in range(3):
+#     print("a")
+#     for j in range(3):
+#         print("b")
+
+# total = 0
+# for i in range(1001):
+#     total += i # total = total + i
+# print("total is:",total)
+
+
+# total = 0
+
+# for i in range(5):
+#     number = int(input('enter a number:> '))
+#     total = total + number  # total += number
+
+# print("total is:", total)
+
+
+# number_of_zeros = 0
+
+# for i in range(5):
+#     new_number = int(input('enter a number:> '))
+#     if new_number == 0:
+#         number_of_zeros += 1
+
+
+# print("number of zeros :", number_of_zeros)
+
+
