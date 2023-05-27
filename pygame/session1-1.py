551efbb7af5c81395ab40bdf4a6a6745b8742ba8
mostafa-sadeghi <msai.sadeghi@gmail.com>
2023-05-27 22:06:03 +0330

    sadeghi'

--- pygame/session1-1.py
+++ pygame/session1-1.py
@@ -28,6 +28,6 @@ while running:
             running = False
 
     display_surface.blit(dragon_left_image, dragon_left_rect)
-    display_surface.blit(dragon_left_image, dragon_left_rect)
+    display_surface.blit(dragon_right_image, dragon_right_rect)
     pygame.display.update()
 pygame.quit()
